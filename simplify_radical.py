cf2721d99817d8d447ae9106c75c98c7443e3575
lunara <98667299+lunaraj@users.noreply.github.com>
2022-03-09 10:48:31 -0500

    Create simplify_radical.py

---
+++ simplify_radical.py
@@ -0,0 +1,24 @@
+import math
+def simplifyRadical(rad):
+    '''
+    Parameters
+    ----------
+    rad : square root
+    Returns
+    -------
+    symplified square root
+    '''
+    squareList = [1]
+    squareFactors = []
+    mult = 1
+    while max(squareList) < rad:
+        squareList.append(mult**2)
+        mult += 1
+    for i in squareList:
+        if rad%i == 0:
+            squareFactors.append(i)
+    del(squareFactors[0])
+    newRad = rad/max(squareFactors)
+    return (int(math.sqrt(max(squareFactors))), int(newRad))
+rad = int(input('type in square root: '))
+print(simplifyRadical(rad))
